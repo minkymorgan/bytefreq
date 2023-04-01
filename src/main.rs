f23c698d281c24a293bd33b8cccc61c989ef7531
Andrew Morgan <andrew@bytesymo.com>
2023-04-01 01:05:30 +0100

    cleaned up the whole project, committing working project that compiles

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,109 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::Path;
 use std::collections::HashMap;
+use std::io::{self, BufRead};
+use rand::prelude::*;
+use chrono::{Local, Datelike, Timelike};
 
-mod parser;
-mod utils;
+fn high_grain_mask(value: &str) -> String {
+    value
+        .chars()
+        .map(|c| match c {
+            'a'..='z' => 'a',
+            'A'..='Z' => 'A',
+            '0'..='9' => '9',
+            _ => c,
+        })
+        .collect()
+}
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: bytefreq-rs <file_path>");
-        return;
-    }
+fn low_grain_mask(value: &str) -> String {
+    let high_grain = high_grain_mask(value);
+    let mut output = String::new();
+    let mut last_char = None;
 
-    let file_path = &args[1];
-    let path = Path::new(file_path);
-
-    if !path.exists() {
-        eprintln!("File not found: {}", file_path);
-        return;
-    }
-
-    let file = File::open(file_path).unwrap();
-    let reader = BufReader::new(file);
-
-    let mut frequency_map: HashMap<String, u64> = HashMap::new();
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-
-        let delimiter = utils::detect_delimiter(&line);
-        let format = utils::detect_format(&line, &delimiter);
-
-        match format {
-            utils::DataFormat::CSV | utils::DataFormat::TSV | utils::DataFormat::PSV => {
-                let fields = parser::parse_delimited_line(&line, &delimiter);
-
-                for field in fields {
-                    *frequency_map.entry(field).or_insert(0) += 1;
-                }
-            }
-            utils::DataFormat::JSON => {
-                let json_data = parser::parse_json_line(&line);
-                for (key, value) in json_data {
-                    *frequency_map.entry(key).or_insert(0) += value;
-                }
-            }
-            _ => {
-                eprintln!("Unsupported file format. Please provide a CSV, TSV, PSV, or JSON file.");
-                return;
-            }
+    for c in high_grain.chars() {
+        if last_char != Some(c) {
+            output.push(c);
+            last_char = Some(c);
         }
     }
 
-    // Output the results
-    println!("Field, Frequency");
-    for (field, count) in &frequency_map {
-        println!("{}, {}", field, count);
+    output
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut frequency_maps: Option<Vec<HashMap<String, usize>>> = None;
+    let mut example_maps: Option<Vec<HashMap<String, String>>> = None;
+    let mut header: Option<Vec<String>> = None;
+    let total_records = stdin
+        .lock()
+        .lines()
+        .filter_map(Result::ok)
+        .enumerate()
+        .inspect(|(i, line)| {
+            if *i == 0 {
+                header = Some(line.split('|').map(String::from).collect());
+                return;
+            }
+
+            let fields = line.split('|').map(String::from).collect::<Vec<String>>();
+
+            if frequency_maps.is_none() {
+                frequency_maps = Some(vec![HashMap::new(); fields.len()]);
+                example_maps = Some(vec![HashMap::new(); fields.len()]);
+            }
+
+            let frequency_maps = frequency_maps.as_mut().unwrap();
+            let example_maps = example_maps.as_mut().unwrap();
+            let mut rng = thread_rng();
+
+            for (idx, field) in fields.iter().enumerate() {
+                let masked_value = low_grain_mask(field);
+                let count = frequency_maps[idx].entry(masked_value.clone()).or_insert(0);
+                *count += 1;
+
+                // Reservoir sampling
+                if rng.gen::<f64>() < 1.0 / (*count as f64) {
+                    example_maps[idx].insert(masked_value.clone(), field.clone());
+                }
+            }
+        })
+        .count();
+
+    let now = Local::now();
+    let now_string = now.format("%Y%m%d %H:%M:%S").to_string();
+    println!();
+    println!("Data Profiling Report: {}", now_string);
+    println!("Examined rows: {}", total_records);
+    println!();
+    println!(
+        "{:<32}\t{:<8}\t{:<8}\t{:<32}",
+        "column", "count", "pattern", "example"
+    );
+    println!("{:-<32}\t{:-<8}\t{:-<8}\t{:-<32}", "", "", "", "");
+
+    if let Some(header) = header {
+        let frequency_maps = frequency_maps.unwrap();
+        let example_maps = example_maps.unwrap();
+        for (idx, name) in header.iter().enumerate() {
+            let mut column_counts = frequency_maps[idx]
+                .iter()
+                .map(|(value, count)| (value, count))
+                .collect::<Vec<(&String, &usize)>>();
+
+            column_counts.sort_unstable_by(|a, b| b.1.cmp(a.1));
+
+            for (value, count) in column_counts {
+                let empty_string = "".to_string();
+                let example = example_maps[idx].get(value).unwrap_or(&empty_string);
+                //let example = example_maps[idx].get(value).unwrap_or(&"".to_string());
+
+                println!(
+                    "col_{:05}_{}\t{:<8}\t{:<8}\t{:<32}",
+                    idx, name, count, value, example
+                );
+            }
+        }
     }
 }
 
