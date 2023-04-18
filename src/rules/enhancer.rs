b543f29ce313875e0158dfbe49d49a2d844b7b8d
Andrew Morgan <andrew@bytesymo.com>
2023-04-18 13:44:42 +0100

    fixing assertion outputs not to be too verbose when no rules fire

--- src/rules/enhancer.rs
+++ src/rules/enhancer.rs
@@ -1,17 +1,16 @@
 use crate::rules::assertions::execute_assertions;
 
-pub fn process_data(data: &serde_json::Value) -> serde_json::Value {
+pub fn process_data(data: &serde_json::Value) -> Option<serde_json::Value> {
     let lu = data["LU"].as_str().unwrap_or("");
     let hu = data["HU"].as_str().unwrap_or("");
     let raw = data["raw"].as_str().unwrap_or("");
 
     let assertions = execute_assertions(raw, lu, hu);
 
-    let mut enhanced_data = assertions.clone();
-    let enhanced_data_obj = enhanced_data.as_object_mut().unwrap();
-    enhanced_data_obj.insert("assertions".to_string(), assertions);
-
-    enhanced_data
+    if assertions.as_object().unwrap().is_empty() {
+        None
+    } else {
+        Some(assertions)
+    }
 }
-
 
