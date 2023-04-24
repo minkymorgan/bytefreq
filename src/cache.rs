52fa3d6a3dce3e8c7e2e1f9447e0bbc6ab64f07c
Andrew Morgan <andrew@bytesymo.com>
2023-04-24 09:25:46 +0100

    created lazy static function to to support memoisation

---
+++ src/cache.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+use std::sync::RwLock;
+
+lazy_static! {
+    pub static ref HANDLE_COUNTRY_NAME_CACHE: RwLock<HashMap<String, Option<(String, String)>>> =
+        RwLock::new(HashMap::new());
+    pub static ref COUNTRY_NAME_TO_ISO3_CACHE: RwLock<HashMap<String, Option<String>>> =
+        RwLock::new(HashMap::new());
+}
+
