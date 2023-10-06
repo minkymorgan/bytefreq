5dbedf2c6e4a21bce55157fa4055e82cfee532f6
andrew <minkymorgan@gmail.com>
2023-10-06 19:22:37 +0000

    testing performance improvements using rayon to parallel process data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-#[macro_use]
-extern crate lazy_static;
+extern crate rayon;
+//extern crate lazy_static;
+use lazy_static::lazy_static;
 use chrono::Local;
 use clap::{App, Arg};
 use rand::prelude::*;
@@ -11,8 +12,12 @@ use unicode_names2;
 use serde_json::json;
 use bytefreq::rules::enhancer::process_data;
 use rayon::prelude::*;
+
+use std::sync::{Arc, Mutex};
 use std::sync::RwLock;
 //use flatten_json_object::Flattener;
+
+
 
 lazy_static! {
     pub static ref HANDLE_COUNTRY_NAME_CACHE: RwLock<HashMap<String, Option<(String, String)>>> =
@@ -512,6 +517,13 @@ fn process_json_line_as_json(json_line: &str, grain: &str) -> serde_json::Value 
 
 
 fn main() {
+
+    // Setup Rayon
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(22) // Using 22 cores out of 24.
+        .build_global()
+        .unwrap();
+
     let matches = App::new("Bytefreq Data Profiler")
         .version("1.0")
         .author("Andrew Morgan <minkymorgan@gmail.com>\n")
@@ -596,6 +608,7 @@ fn main() {
     )
         .get_matches();
 
+
     let report = matches.value_of("report").unwrap();
     let enhanced_output = matches.is_present("enhanced_output");
     let flat_enhanced = matches.is_present("flat_enhanced");
@@ -614,19 +627,24 @@ fn main() {
 
         // new code to process tabular or json data
         let stdin = io::stdin();
-        let mut frequency_maps: Vec<HashMap<String, usize>> = Vec::new();
-        let mut example_maps: Vec<HashMap<String, String>> = Vec::new();
-        let mut column_names: HashMap<String, usize> = HashMap::new();
-        let mut field_count_map: HashMap<usize, usize> = HashMap::new();
-        let mut record_count: usize = 0;
+
+        // shared mutable state wrapped in Mutex and Arc to aid parallel processing in rayon
+        let frequency_maps: Arc<Mutex<Vec<HashMap<String, usize>>>> = Arc::new(Mutex::new(Vec::new()));
+        let example_maps: Arc<Mutex<Vec<HashMap<String, String>>>> = Arc::new(Mutex::new(Vec::new()));
+        let column_names: Arc<Mutex<HashMap<String, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+        let field_count_map: Arc<Mutex<HashMap<usize, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+        let record_count: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+
         let pathdepth = matches
             .value_of("pathdepth")
             .unwrap()
             .parse::<usize>()
             .unwrap();
-        //let remove_array_numbers = matches.value_of("remove_array_numbers").unwrap() != "false";
 
-        for line in stdin.lock().lines().filter_map(Result::ok) {
+        let lines: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
+
+        // Now we move the loop into a parallel iterator
+        lines.par_iter().for_each(|line| {
             if !line.is_empty() {
                 if format == "json" {
                     if enhanced_output == true {
@@ -639,20 +657,31 @@ fn main() {
                             Ok(flattened) => println!("{}",                           // significantly unnests data, line by line 
                             serde_json::to_string(&flattened).unwrap()),
                             Err(e) => eprintln!("Failed to flatten JSON: {}", e), 
-                            } 
+                        } 
                     } else {
+                        let mut local_frequency_maps = frequency_maps.lock().unwrap();
+                        let mut local_example_maps = example_maps.lock().unwrap();
+                        let mut local_column_names = column_names.lock().unwrap();
                         process_json_line(
                             &line,
-                            &mut frequency_maps,
-                            &mut example_maps,
+                            &mut local_frequency_maps,
+                            &mut local_example_maps,
                             grain,
-                            &mut column_names,
+                            &mut local_column_names,
                             pathdepth,
                             remove_array_numbers,
                         );
                     }
                 } else {
-                    if record_count == 0 {
+                    // Tabular processing
+
+                    let mut local_column_names = column_names.lock().unwrap();
+                    let mut local_record_count = record_count.lock().unwrap();
+                    let mut local_frequency_maps = frequency_maps.lock().unwrap();
+                    let mut local_example_maps = example_maps.lock().unwrap();
+                    let mut local_field_count_map = field_count_map.lock().unwrap();
+
+                    if *local_record_count == 0 {
                         let header = line; //+ delimiter + "Err1" + delimiter + "Err2";
                                            // Process header for tabular data
                         for (idx, name) in header
@@ -660,27 +689,30 @@ fn main() {
                             .map(|s| s.trim().replace(" ", "_"))
                             .enumerate()
                         {
-                            column_names.insert(name.to_string(), idx);
-                            frequency_maps.push(HashMap::new());
-                            example_maps.push(HashMap::new());
+                            local_column_names.insert(name.to_string(), idx);
+                            local_frequency_maps.push(HashMap::new());
+                            local_example_maps.push(HashMap::new());
                         }
                     } else {
                         // Process tabular data
-                        if !column_names.is_empty() {
+                        if !local_column_names.is_empty() {
                             let fields = line.split(delimiter).collect::<Vec<&str>>();
                             let mut processed_fields = Vec::new();
 
                             for (i, field) in fields.iter().enumerate() {
-                                let column_name = match column_names.iter().find(|(_, &v)| v == i) {
+                                let column_name = match local_column_names.iter().find(|(_, &v)| v == i) {
                                     Some((name, _)) => name.clone(),
                                     None => {
-                                        let extra_column_index = i + 1 - column_names.len();
+                                        let extra_column_index = i + 1 - local_column_names.len();
                                         let new_name = format!("RaggedErr{}", extra_column_index);
 
                                         // Update column_names, frequency_maps, and example_maps for the new column
-                                        column_names.insert(new_name.clone(), column_names.len());
-                                        frequency_maps.push(HashMap::new());
-                                        example_maps.push(HashMap::new());
+                                        //local_column_names.insert(new_name.clone(), local_column_names.len());
+                                        let current_length = local_column_names.len();
+                                        local_column_names.insert(new_name.clone(), current_length);
+
+                                        local_frequency_maps.push(HashMap::new());
+                                        local_example_maps.push(HashMap::new());
 
                                         new_name
                                     }
@@ -689,13 +721,14 @@ fn main() {
                             }
 
                             let field_count = processed_fields.len();
-                            *field_count_map.entry(field_count).or_insert(0) += 1;
+                            let mut field_count_map_guard = field_count_map.lock().unwrap();
+                            *field_count_map_guard.entry(field_count).or_insert(0) += 1;
 
                             for (name, value) in &processed_fields {
                                 let masked_value = mask_value(value, grain, &name);
 
-                                if let Some(idx) = column_names.get(name) {
-                                    let count = frequency_maps[*idx]
+                                if let Some(idx) = local_column_names.get(name) {
+                                    let count = local_frequency_maps[*idx]
                                         .entry(masked_value.clone())
                                         .or_insert(0);
                                     *count += 1;
@@ -703,7 +736,7 @@ fn main() {
                                     // Reservoir sampling
                                     let mut rng = thread_rng();
                                     if rng.gen::<f64>() < 1.0 / (*count as f64) {
-                                        example_maps[*idx]
+                                        local_example_maps[*idx]
                                             .insert(masked_value.clone(), value.to_string());
                                     }
                                 } else {
@@ -717,7 +750,7 @@ fn main() {
 
                             // collect tabular data to enhance, enhance, print
                             if enhanced_output {
-                                let processed_fields: Vec<(String, String)> = column_names.iter().map(|column_name| {
+                                let processed_fields: Vec<(String, String)> = local_column_names.iter().map(|column_name| {
                                     let value = fields[*column_name.1].to_string();
                                     (column_name.0.clone(), value)
                                 }).collect();
@@ -726,7 +759,7 @@ fn main() {
                                 //let enhanced_json_line = process_data(&json_line);
                                 println!("{}", serde_json::to_string(&json_line).unwrap());
                             } else if flat_enhanced {
-                                let processed_fields: Vec<(String, String)> = column_names.iter().map(|column_name| {
+                                let processed_fields: Vec<(String, String)> = local_column_names.iter().map(|column_name| {
                                     let value = fields[*column_name.1].to_string();
                                     (column_name.0.clone(), value)
                                 }).collect();
@@ -735,14 +768,15 @@ fn main() {
                                 match flatten_json_object::Flattener::new().flatten(&json_line) {
                                     Ok(flattened) => println!("{}", serde_json::to_string(&flattened).unwrap()),
                                     Err(e) => eprintln!("Failed to flatten tabular JSON: {}", e),
-                                }
-                            }
-                        }
-                    } //end of else
+                                }       // 1
+                            }           // 2
+                        }               // 3
+                    }                   // 4
+                    *local_record_count += 1;
                 }
-                record_count += 1;
+                
             }
-        }
+        });
 
         // Output the processed tabular line in JSON format if the enhanced_output flag is set to true
 
@@ -751,11 +785,13 @@ fn main() {
             let now_string = now.format("%Y%m%d %H:%M:%S").to_string();
             println!();
             println!("Data Profiling Report: {}", now_string);
-            println!("Examined rows: {}", record_count);
+            let record_count_value = record_count.lock().unwrap();
+            println!("Examined rows: {}", record_count_value);
             println!();
             println!("FieldsPerLine:");
             // Print the field count map
-            for (field_count, frequency) in &field_count_map {
+            let field_count_map_ref = field_count_map.lock().unwrap();
+            for (field_count, frequency) in &*field_count_map_ref {
                 println!("{} fields: {} rows", field_count, frequency);
             }
 
@@ -767,11 +803,14 @@ fn main() {
             println!("{:-<32}\t{:-<8}\t{:-<8}\t{:-<32}", "", "", "", "");
 
             // sort the reporting lines
-            let mut sorted_column_names: Vec<(&String, &usize)> = column_names.iter().collect();
+            let column_names_ref = column_names.lock().unwrap();
+            let mut sorted_column_names: Vec<(&String, &usize)> = column_names_ref.iter().collect();
+
             sorted_column_names.sort_unstable_by_key(|(_, idx)| **idx);
 
             for (name, idx) in sorted_column_names { 
-                if let Some(frequency_map) = frequency_maps.get(*idx) {
+                let frequency_maps_ref = frequency_maps.lock().unwrap();
+                if let Some(frequency_map) = frequency_maps_ref.get(*idx) {
                     let mut column_counts = frequency_map
                         .iter()
                         .map(|(value, count)| (value, count))
@@ -781,7 +820,8 @@ fn main() {
 
                     for (value, count) in column_counts {
                         let empty_string = "".to_string();
-                        let example = example_maps[*idx].get(value).unwrap_or(&empty_string);
+                        let example_maps_ref = example_maps.lock().unwrap();
+                        let example = example_maps_ref[*idx].get(value).unwrap_or(&empty_string);
 
                         println!(
                             "col_{:05}_{}\t{:<8}\t{:<8}\t{:<32}",
